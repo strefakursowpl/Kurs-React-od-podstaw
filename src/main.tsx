2d76ab71c3bc4e5c3a05e80159d28b1d5f93bcbd
Sebastian Machaj <team@blackredstudio.com>
2025-04-11 18:51:34 +0200

    3_7 - Kontekst w aplikacji - hook useContext

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import './index.css';
-import { UseEffectComponent } from "./use-effect";
+import { ContextComponent } from "./context";
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<UseEffectComponent />
+		<ContextComponent />
 	</StrictMode>,
 );
