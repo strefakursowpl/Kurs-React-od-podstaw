d2217f3f5c52a9d6f39c39d433d8d0c16a01df95
Sebastian Machaj <team@blackredstudio.com>
2025-04-16 18:40:31 +0200

    3_11 - React Compiler

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client';
 
 import './index.css';
 import { Main } from "./optimization";
+import { Counter } from "./state";
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<Main />
+		<Counter />
 	</StrictMode>,
 );
