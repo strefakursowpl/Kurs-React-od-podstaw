fb65b2e2034ed5eb1b100d2698eb672982ae2d2f
Sebastian Machaj <team@blackredstudio.com>
2025-04-19 13:24:56 +0200

    4_3 - Przygotowanie do pracy

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import './index.css';
-import { Main } from "./optimization";
-import { Counter } from "./state";
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<Counter />
+		<div className="text-primary">
+			Test
+		</div>
 	</StrictMode>,
 );
